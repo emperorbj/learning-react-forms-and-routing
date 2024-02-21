f752449eebb798a0931c72e6f96188da73fb6332
BOLAJI OPATOLA <138551605+emperorbj@users.noreply.github.com>
2024-02-20 19:26:32 -0800

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Context from '../Context'
 
 const About = () => {
     return (
@@ -6,6 +7,7 @@ const About = () => {
             <h1>About us</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique facilis rerum minus, 
             totam eligendi molestiae veniam eius dolor possimus unde et minima, dolore ad id eum impedit in temporibus vitae.</p>
+            <Context/>
         </div>
     )
 }
