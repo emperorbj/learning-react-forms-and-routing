9332d93915bfc1b6790d1f89117c183516b0d3f4
BOLAJI OPATOLA <138551605+emperorbj@users.noreply.github.com>
2024-02-20 19:23:27 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,32 @@
+import { useState , createContext} from 'react'
+import './App.css'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import About from './Components/About'
+import Gadgets from './Components/Gadgets'
+import Services from './Components/Services'
+import Header from './Components/Header'
 
+export const  TheContext = createContext()
+
+function App() {
+  const [name,setName] = useState('CEO Bolaji')
+  return (
+    <>
+    <TheContext.Provider value={name}>
+            {/* <BrowserRouter>
+
+        <Header/>
+        <Routes>
+
+        <Route path='/about' element={<About/>}/>
+        <Route path='/gadgets' element={<Gadgets/>}/>
+        <Route path='/services' element={<Services/>}/>
+
+        </Routes>
+
+        </BrowserRouter> */}
+        <About/>
+    </TheContext.Provider>
+    </>
+  )
+}
